1f5c7a120abee8ab6cc6824733b393465fb0339d
xingthant <xerlokxerlok322@gmail.com>
2025-09-01 16:37:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ cloudinary.config({
 // Middleware - ORDER IS IMPORTANT!
 app.use(cors({
   origin: 'https://billion-star-front.vercel.app',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 app.use(express.json());
 app.use(cookieParser());
