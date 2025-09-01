b9f72ef1f1ea327daddc10e691679203242a5b3e
xingthant <xerlokxerlok322@gmail.com>
2025-09-01 15:04:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const cloudinary = require('cloudinary').v2;
+const allowedOrigins = [
+  'https://billion-star-front.vercel.app',
+  'https://billion-star-front.vercel.app/'
+];
+
 
 // Load environment variables from .env file
 dotenv.config();
@@ -22,9 +27,17 @@ cloudinary.config({
 
 // Middleware - ORDER IS IMPORTANT!
 app.use(cors({
-  origin: 'https://billion-star-front.vercel.app',
-  credentials: true
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow REST tools or server-to-server requests without origin
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
